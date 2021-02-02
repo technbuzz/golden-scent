5b18515672cb56cbc7abce449609f201715a89d1
Samiullah Khan <samiullah.khan.1989@gmail.com>
2021-02-02 22:30:04 +0400

    Product Card Design Done ✅ Basic card design is implemented, for the time being fetch api is used to fetch data so than more than one product are compared

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,13 +1,32 @@
 <template>
-  <product />
+  <div>
+    <h1>Product List</h1>
+    <product :product="product" v-for="(product, index) in products" v-bind:key="index"/>
+  </div>
 </template>
 
 <script>
 import Product from './product.vue'
 export default {
   name: 'ProductList',
+  data () {
+    return {
+      products: []
+    }
+  },
+  async created () {
+    await this.fetchProducts()
+  },
   components: {
     Product
+  },
+  methods: {
+    async fetchProducts () {
+      const res = await fetch('https://gs-euw1-public-data-prod.s3-eu-west-1.amazonaws.com/new-web/test/test.json')
+      const { hits } = await res.json()
+      this.products = hits
+      console.log('products: ', hits);
+    }
   }
 }
 </script>
--- src/components/product.vue
+++ src/components/product.vue
@@ -1,22 +1,50 @@
 <template>
   <div>
-    <b-container class="bv-example-row">
-      <b-row>
-        <b-col>1 of 2</b-col>
-        <b-col>2 of 2</b-col>
-      </b-row>
+    <b-card
+      title="Card Title"
+      :img-src="product.image_url"
+      img-alt="Image"
+      img-top
+      tag="article"
+      no-body
+      style="max-width: 20rem;"
+    >
+      <b-card-body class="p-2">
+        <b-card-title>{{ product.name }}</b-card-title>
+        <b-card-text class="text-black-50">
+          Some quick example text to build on the card title and make up the bulk of the card's
+          content.
+        </b-card-text>
+        <b-container class="bv-example-row">
+          <b-row class="justify-content-between">
+            <h6 class="price font-weight-bold">{{ product.special_price }} SAR</h6>
+            <h6><b-badge>-{{ product.special_price }} %</b-badge></h6>
+          </b-row>
 
-      <b-row>
-        <b-col>1 of 3</b-col>
-        <b-col>2 of 3</b-col>
-        <b-col>3 of 3</b-col>
-      </b-row>
-    </b-container>
+          <b-row class="justify-content-between">
+            <p class="small"><s>444 SAR</s></p>
+            <h6>Save 285.0 SAR</h6>
+          </b-row>
+        </b-container>
+      </b-card-body>
+
+      <b-button href="#" block variant="primary">Add</b-button>
+      <!-- <pre>{{product}}</pre> -->
+    </b-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Product'
+  name: 'Product',
+  props: {
+    product: Object
+  }
 }
 </script>
+
+<style lang="scss">
+  .price {
+    color: red
+  }
+</style>
