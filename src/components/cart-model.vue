12ac718d5573d9cd13afc2c3ca1a58bd57e80bfc
Samiullah Khan <samiullah.khan.1989@gmail.com>
2021-02-03 15:06:55 +0400

    🛒 Modal Design Implemented Modal content moved into its own component. Basic content is also added

--- src/components/cart-model.vue
+++ src/components/cart-model.vue
@@ -1,5 +1,20 @@
 <template>
-  <h1>Cart Modal</h1>
+  <div>
+    <b-media class="mb-5">
+      <template #aside>
+        <b-img :src="product.thumbnail_url" width="64" alt="placeholder"></b-img>
+      </template>
+
+      <h6 class="m-0">{{ product.brand_value }}</h6>
+      <p class="m-0 font-weight-light">{{ product.name }}</p>
+      <h5>{{ product.special_price }}</h5>
+      <a href="#" class="text-info">View Porduct Details</a>
+    </b-media>
+    <hr>
+    <h4>Size</h4>
+    <b-button href="#" block variant="primary btn-lg" @click="showModal">Add To Cart</b-button>
+
+  </div>
 </template>
 
 <script>
--- src/components/product.vue
+++ src/components/product.vue
@@ -1,45 +1,69 @@
 <template>
-  <b-card
-    title="Card Title"
-    :img-src="product.image_url"
-    img-alt="Image"
-    img-top
-    tag="article"
-    no-body
-    style="max-width: 20rem;"
-  >
-    <b-card-body class="px-3 py-1">
-      <b-card-title class="mb-1">{{ product.brand_value }}</b-card-title>
-      <b-card-text class="text-black-50 font-weight-light">
-        {{ product.name }}
-      </b-card-text>
-      <b-container class="bv-example-row">
-        <b-row class="justify-content-between">
-          <h6 class="price font-weight-bold">{{ product.special_price }} SAR</h6>
-          <h6><b-badge class="badge-cart">-{{ product.special_price }} %</b-badge></h6>
-        </b-row>
+  <div>
+    <b-card
+      title="Card Title"
+      :img-src="product.image_url"
+      img-alt="Image"
+      img-top
+      tag="article"
+      no-body
+      style="max-width: 20rem;"
+    >
+      <b-card-body class="px-3 py-1">
+        <b-card-title class="mb-1">{{ product.brand_value }}</b-card-title>
+        <b-card-text class="text-black-50 font-weight-light">
+          {{ product.name }}
+        </b-card-text>
+        <b-container class="bv-example-row">
+          <b-row class="justify-content-between">
+            <h6 class="price font-weight-bold">{{ product.special_price }} SAR</h6>
+            <h6><b-badge class="badge-cart">-{{ product.special_price }} %</b-badge></h6>
+          </b-row>
 
-        <b-row class="justify-content-between">
-          <p class="small"><s>444 SAR</s></p>
-          <h6>Save 285.0 SAR</h6>
-        </b-row>
-      </b-container>
-    </b-card-body>
+          <b-row class="justify-content-between">
+            <p class="small"><s>444 SAR</s></p>
+            <h6>Save 285.0 SAR</h6>
+          </b-row>
+        </b-container>
+      </b-card-body>
 
-    <div class="px-3 pb-2"><b-button href="#" block variant="primary">Add</b-button></div>
-  </b-card>
+      <div class="px-3 pb-2"><b-button href="#" block variant="primary" @click="showModal">Add</b-button></div>
+    </b-card>
+
+    <b-modal v-model="cartModel" title="Add item to cart" hide-footer>
+      <cart-model :product="product" />
+    </b-modal>
+  </div>
 </template>
 
 <script>
+import CartModel from './cart-model.vue'
+
 export default {
   name: 'Product',
   props: {
     product: Object
+  },
+  data () {
+    return {
+      cartModel: false
+    }
+  },
+  components: {
+    CartModel
+  },
+  methods: {
+    showModal () {
+      this.cartModel = true
+    },
+    hideModal () {
+      this.cartModel = false
+    }
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .price {
     color: red
   }
